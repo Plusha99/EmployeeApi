844876726bb7409d488dcb128ec13a80dbf0d41b
Мартынов Илья Игоревич <ilya.martynov@osinit.com>
2023-04-07 11:13:00 +0300

    Добавьте файлы проекта.

---
+++ EmployeeApi/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeApi/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhineNumber { get; set; } = string.Empty;
+    }
+}
